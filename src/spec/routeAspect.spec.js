3fbb66d59a34b24773b97cec0e65dbe63bf766c9
Kevin Bridges <kevin@wiiBridges.com>
2018-03-02 10:16:02 -0600

    add diagnostic logs and defensive coding

--- src/spec/routeAspect.spec.js
+++ src/spec/routeAspect.spec.js
@@ -1,10 +1,34 @@
-import {routeAspect}  from '..'; // STOP USING: '../../tooling/ModuleUnderTest';
+import {routeAspect}  from '..'; // module under test
+import {logf}         from '../routeAspect'; // ?? JUST to enable logs
+
+// temporarly turn on logging (just for fun)
+logf.enable(); // ??
 
 describe('routeAspect() tests', () => {
 
-  test('name', () => {
-    expect( routeAspect.name)
-      .toEqual('route');
+  describe('validate routeAspect.name', () => {
+    test('name', () => {
+      expect( routeAspect.name)
+        .toEqual('route');
+    });
   });
+
+
+  describe('genesis()', () => {
+
+    test('requires fallbackElm to be configured', () => {
+      expect(routeAspect.genesis())
+        .toMatch(/aspect requires fallbackElm to be configured/);
+    });
+
+    // ?? due to the way BLA must be in seperate test module
+    test.skip('fallbackElm supplied', () => {
+      routeAspect.fallbackElm = 'my simulated fallbackElm';
+      expect(routeAspect.genesis())
+        .toBe(null);
+    });
+
+  });
+
 
 });
